f50e46c0d6d4facaf33757594b19f675b96b4665
Julius Pahama <pahamajulius@gmail.com>
2026-06-04 07:56:02 +0800

    fix(tests): update category creation to use find_or_create_by!; adjust expense date handling; streamline api url

--- backend/spec/factories/expenses.rb
+++ backend/spec/factories/expenses.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :expense do
     description { "MyString" }
     amount { "9.99" }
+    date { Date.today }
     category { nil }
     payer_name { "MyString" }
   end
